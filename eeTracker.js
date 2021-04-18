b8b306acf8470a0ce13779e323bb1d235cc17b1b
J_Kiser <jdkiser228@gmail.com>
2021-04-18 12:48:10 -0400

    updating readme file

--- eeTracker.js
+++ eeTracker.js
@@ -36,15 +36,13 @@ function startTracking() {
                     "Update Employee Role",
                     "Update Employee Manager",
                     "View All Roles",
-                    "Delete Role",
-                    "Delete Department",
                     "Exit App"
                 ],
             },
         ])
 
         .then(function (answer) {
-            switch (answer.generate) {
+            switch (answer.Generate) {
                 case "View All Employee Information":
                     displayAllInfo();
                     break;
@@ -71,12 +69,6 @@ function startTracking() {
                     break;
                 case "View All Roles":
                     viewAllRoles();
-                    break;
-                case "Delete Role":
-                    deleteRole();
-                    break;
-                case "Delete Department":
-                    deleteDept();
                     break;
                 case "Exit App":
                     connection.end();
@@ -108,7 +100,7 @@ function viewAllEmployees() {
 
 // function to view all departments
 function viewAllbyDept() {
-    connection.query("SELECT * FROM department.name", function (err, res) {
+    connection.query("SELECT * FROM department", function (err, res) {
         if (err) throw err;
         console.table(res);
         startTracking();
@@ -117,7 +109,7 @@ function viewAllbyDept() {
 
 // function to view all employees by manager 
 function viewAllbyManager() {
-    connection.query("SELECT * FROM employee.manager_id", function (err, res) {
+    connection.query("SELECT * FROM manager_id", function (err, res) {
         if (err) throw err;
         console.table(res);
         startTracking();
@@ -128,193 +120,228 @@ function viewAllbyManager() {
 function addEmployee() {
     console.log("Follow prompts to add new employee:\n");
     inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "Enter employee's first name:",
-          name: "employee_first_name",
-        },
-        {
-          type: "input",
-          message: "Enter employee's last name:",
-          name: "employee_last_name",
-        },
-        {
-          type: "input",
-          message: "Enter employee's single digit role id number:",
-          name: "role_id",
-        },
-        {
-          type: "confirm",
-          message: "Is the employee a manager?",
-          name: "manager",
-        },
-      ])
-      .then(function (answer) {
-        const firstName = answer.employee_first_name;
-        const lastName = answer.employee_last_name;
-        const roleId = answer.role_id;
-        const isManager = answer.manager;
-  
-        if (isManager) {
-          inquirer
-            .prompt([
-              {
-                type: "input",
-                message: "What is the manager ID?",
-                name: "manager_id",
-              },
-            ])
-            .then(function (answer) {
-              var query = connection.query(
-                "INSERT INTO employee SET ?",
-                {
-                  first_name: firstName,
-                  last_name: lastName,
-                  role_id: roleId,
-                  manager_id: answer.manager_id,
-                },
-                function (err, res) {
-                  if (err) throw err;
-                  console.log(
-                    res.affectedRows +
-                      " New Employee has been successfully added!\n"
-                  );
-                  startTracking();
-                }
-              );
-            });
-        } else {
-          var query = connection.query(
-            "INSERT INTO employee SET ?",
+        .prompt([
             {
-              first_name: answer.employee_first_name,
-              last_name: answer.employee_last_name,
-              role_id: answer.role_id,
+                type: "input",
+                message: "Enter employee's first name:",
+                name: "employee_first_name",
             },
-            function (err, res) {
-              if (err) throw err;
-              console.log(
-                res.affectedRows + " New Employee has been successfully added!\n"
-              );
-              startTracking();
+            {
+                type: "input",
+                message: "Enter employee's last name:",
+                name: "employee_last_name",
+            },
+            {
+                type: "input",
+                message: "Enter employee's single digit role id number:",
+                name: "role_id",
+            },
+            {
+                type: "confirm",
+                message: "Is the employee a manager?",
+                name: "manager",
+            },
+        ])
+        .then(function (answer) {
+            const firstName = answer.employee_first_name;
+            const lastName = answer.employee_last_name;
+            const roleId = answer.role_id;
+            const isManager = answer.manager;
+
+            if (isManager) {
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: "What is the manager ID?",
+                            name: "manager_id",
+                        },
+                    ])
+                    .then(function (answer) {
+                        var query = connection.query(
+                            "INSERT INTO employee SET ?",
+                            {
+                                first_name: firstName,
+                                last_name: lastName,
+                                role_id: roleId,
+                                manager_id: answer.manager_id,
+                            },
+                            function (err, res) {
+                                if (err) throw err;
+                                console.log(
+                                    res.affectedRows +
+                                    " New Employee has been successfully added!\n"
+                                );
+                                startTracking();
+                            }
+                        );
+                    });
+            } else {
+                var query = connection.query(
+                    "INSERT INTO employee SET ?",
+                    {
+                        first_name: answer.employee_first_name,
+                        last_name: answer.employee_last_name,
+                        role_id: answer.role_id,
+                    },
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log(
+                            res.affectedRows + " New Employee has been successfully added!\n"
+                        );
+                        startTracking();
+                    }
+                );
             }
-          );
-        }
-      });
-  }
+        });
+}
 
 //   function to remove an employee
-  function removeEmployee() {
+function removeEmployee() {
     console.log("Follow prompt to delete Employee: \n");
     inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "Enter the employee ID to be deleted?",
-          name: "delete_employee",
-        },
-      ])
-      .then(function (answer) {
-        var employee = answer.delete_employee;
-        connection.query(
-          "DELETE FROM employee WHERE ?",
-          {
-            id: employee,
-          },
-          function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + " employee deleted!\n");
-            startTracking();
-          }
-        );
-      });
-  }
+        .prompt([
+            {
+                type: "input",
+                message: "Enter the employee ID to be deleted?",
+                name: "delete_employee",
+            },
+        ])
+        .then(function (answer) {
+            var employee = answer.delete_employee;
+            connection.query(
+                "DELETE FROM employee WHERE ?",
+                {
+                    id: employee,
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(res.affectedRows + " employee deleted!\n");
+                    startTracking();
+                }
+            );
+        });
+}
 
 //   function to update employee role
-  function updateEmployeeRole() {
+function updateEmployeeRole() {
     console.log("Follow prompts to update Role: \n");
     inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "Enter employee's id:",
-          name: "employee_id",
-        },
-      ])
-      .then(function (answer) {
-        const employeeId = answer.employee_id;
-        if(employeeId){
-        inquirer
-          .prompt([
+        .prompt([
             {
-              type: "list",
-              message: "Enter employee new role:",
-              choices: [
-                "Director",
-                "Finance/Dept Manager",
-                "Team Manager",
-                "Representative",
-                "Sales Support",
-                "Human Resources"
-              ],
-              name: "role_type",
+                type: "input",
+                message: "Enter employee's id:",
+                name: "employee_id",
             },
-            {
-              type: "input",
-              message: "Enter new salary:",
-              name: "salary",
-            },
-            {
-              type: "input",
-              message: "Enter department ID:",
-              name: "dept_id",
-            },
-          ])
-          .then(function (answer) {
-            const roleType = answer.role_type;
-            const salary = answer.salary;
-            const deptId = answer.dept_id;
-  
-            var query = connection.query(
-              "UPDATE role SET ? WHERE ?",
-              [
-                {
-                  title: roleType,
-                  salary: salary,
-                  department_id: deptId,
-                },
-                {
-                  id: employeeId,
-                },
-              ],
-              function (err, res) {
-                if (err) throw err;
-                console.log(res.affectedRows + " Role updated!\n");
-                // Call deleteRole AFTER the UPDATE completes
-                viewRole();
-                //deleteRole();
-              }
-            );
-          });
-        }
-      });
-    }
+        ])
+        .then(function (answer) {
+            const employeeId = answer.employee_id;
+            if (employeeId) {
+                inquirer
+                    .prompt([
+                        {
+                            type: "list",
+                            message: "Enter employee new role:",
+                            choices: [
+                                "Director",
+                                "Finance/Dept Manager",
+                                "Team Manager",
+                                "Representative",
+                                "Sales Support",
+                                "Human Resources"
+                            ],
+                            name: "role_type",
+                        },
+                        {
+                            type: "input",
+                            message: "Enter new salary:",
+                            name: "salary",
+                        },
+                        {
+                            type: "input",
+                            message: "Enter department ID:",
+                            name: "dept_id",
+                        },
+                    ])
+                    .then(function (answer) {
+                        const roleType = answer.role_type;
+                        const salary = answer.salary;
+                        const deptId = answer.dept_id;
+
+                        var query = connection.query(
+                            "UPDATE role SET ? WHERE ?",
+                            [
+                                {
+                                    title: roleType,
+                                    salary: salary,
+                                    department_id: deptId,
+                                },
+                                {
+                                    id: employeeId,
+                                },
+                            ],
+                            function (err, res) {
+                                if (err) throw err;
+                                console.log(res.affectedRows + " Role updated!\n");
+                            }
+                        );
+                    });
+            }
+        });
+}
 
 
 // Need to insert update manager
+async function updateEmployeeManager() {
+    let emp = [];
 
+    await connection.query("SELECT * FROM employee", function (err, res) {
+        //console.log(res);
+        emp = res;
+
+        const empChoice = emp.map(({ id, first_name, last_name }) => ({
+            name: first_name + " " + last_name,
+            value: id
+        }));
+
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    message: "Select employee you want to update manager for",
+                    choices: empChoice,
+                    name: "empId",
+                }]).then(function (res) {
+                    const m = connection.query("SELECT * FROM employee WHERE id !=" + parseInt(res.empId));
+                    const mChoice = m.map(({ id, first_name, last_name }) => ({
+                        name: first_name + " " + last_name,
+                        value: id
+                    }));
+
+                    //     inquirer
+                    //     .prompt([
+                    // {
+
+                    //   type: "list",
+                    //   message: "Select manager",
+                    //   choices: mChoice,
+                    //   name: "mId",
+                    // }]).then(function(mres){
+                    //     connection.query("UPDATE employee SET manager_id="+parseInt(mres.mId)+" WHERE id="+parseInt(res.empId), function(err, data){
+                    //         if(err) throw err;
+                    //         startTracking();
+                    //     });
+                    //  });
+                })
+    });
+
+
+}
 // Function to view all roles
-    function viewAllRoles() {
-        connection.query("SELECT * FROM role", function (err, res) {
-          if (err) throw err;
-          console.table(res);
-          generateApp();
-        });
-      }
-
-// Function to delete role
-
-// Function to delete department
-
-// Function to exit app
+function viewAllRoles() {
+    connection.query("SELECT * FROM role", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        startTracking();
+    });
+}
